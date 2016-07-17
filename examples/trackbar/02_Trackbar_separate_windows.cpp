e2e28acc9de021bf2e7db0bdab4283cc35166ec1
Amir Bawab <amirbawab@gmail.com>
2016-07-17 00:24:12 -0400

    Added trackbar examples

---
+++ examples/trackbar/02_Trackbar_separate_windows.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv ) {
+
+	// Create matrix from image
+	Mat imageSrc = imread("lena1.png", CV_LOAD_IMAGE_COLOR);
+	Mat imageDest;
+
+	// Create windows
+	namedWindow("My window 1", WINDOW_AUTOSIZE);
+	namedWindow("My window 2", WINDOW_AUTOSIZE);
+
+	// Create trackbar
+	int brightnessValue = 50;
+	createTrackbar("Brightness", "My window 2", &brightnessValue, 255);
+
+	// Keep updating the image
+	while (true) {
+
+		 // Set brightness
+		 imageDest = imageSrc + Scalar(brightnessValue, brightnessValue, brightnessValue);
+
+		 // Show image in window
+		 imshow("My window 1", imageDest);
+
+		 // Delay 50ms
+		 waitKey(50);
+	}
+
+	return 0;
+}
+
